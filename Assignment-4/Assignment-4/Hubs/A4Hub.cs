582c3fb8252c3c100c0e8f527d5a008b7847615c
colin.heald@aecom.com <colin.heald@aecom.com>
2018-12-03 18:39:46 -0700

    added signalR stuff

---
+++ Assignment-4/Assignment-4/Hubs/A4Hub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace Assignment_4.Hubs
+{
+	public class MyHub : Hub
+	{
+		public async Task SendMessage(string rank)
+		{
+			
+			await Clients.All.SendAsync();
+		}
+	}
+}
